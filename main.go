f919c5b4d9c09dcbfc5899f829ff0835f755b310
Oluwafemi Ajayi <ajayioluwafemi2517@gmail.com>
2026-07-15 07:27:15 -0700

    Add calculator Operation

--- main.go
+++ main.go
@@ -3,5 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Calculator!")
+	var num1, num2 float64
+	var operator string
+
+	// Get the first number
+	fmt.Print("Enter first number: ")
+	fmt.Scan(&num1)
+
+	// Get the operator
+	fmt.Print("Enter operation (+, -, *, /): ")
+	fmt.Scan(&operator)
+
+	// Get the second number
+	fmt.Print("Enter second number: ")
+	fmt.Scan(&num2)
+
+	// Perform the calculation
+	switch operator {
+	case "+":
+		fmt.Println("Result:", num1+num2)
+
+	case "-":
+		fmt.Println("Result:", num1-num2)
+
+	case "*":
+		fmt.Println("Result:", num1*num2)
+
+	case "/":
+		if num2 == 0 {
+			fmt.Println("Error: Cannot divide by zero.")
+		} else {
+			fmt.Println("Result:", num1/num2)
+		}
+
+	default:
+		fmt.Println("Invalid operator.")
+	}
 }
